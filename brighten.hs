915c8bade188e717dfdf870a27f32e3e5ef04e3f
dan soucy <soucydi194@potsdam.edu>
2015-07-16 20:30:36 -0400

    added command-line args to brighten.hs

--- brighten.hs
+++ brighten.hs
@@ -1,11 +1,13 @@
 -- this program would prabably be better with IO.Strict.
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE BangPatterns #-}
+{-# OPTIONS_GHC -Wall #-}
 import System.Environment (getArgs)
+import System.IO
+import Text.Read (readMaybe)
 
 path :: FilePath
--- path = "/sys/class/backlight/intel_backlight/brightness"
-path = "/tmp/HSTEST"
+path = "/sys/class/backlight/intel_backlight/brightness"
 
 max_brightness, increment :: Int
 max_brightness = 7812
@@ -13,21 +15,34 @@ increment = 500
 
 main :: IO ()
 main = do
-    current_bright <- readFile path
-    let new_bright = show $ brighten (read current_bright)
-    putStrLn new_bright
-    writeFile path new_bright
-{- -- alternate implementation, only opens the file once but is uglier...
-main = do
+-- only opens the file once but is uglier...
+    args <- getArgs
     handle <- openFile path ReadWriteMode
     current_bright <- read <$> hGetLine handle
-    let new_bright = darken current_bright
-    print new_bright
+    let new_bright = show $ calc current_bright args
+--     putStrLn new_bright
     hSeek handle AbsoluteSeek 0
-    hPutStrLn handle (show new_bright)
-    hSetFileSize handle (fromIntegral (length (show new_bright) + 1))
+    hPutStrLn handle (new_bright)
+    hSetFileSize handle (fromIntegral (length (new_bright) + 1))
     hClose handle
+{-
+-- this implementation looks nice, but i think it causes a race condition... 
+    args <- getArgs
+    current_bright <- read <$> readFile path
+    let new_bright = show $ calc current_bright args
+    putStrLn new_bright
+    writeFile path new_bright
 -}
+
+
+calc :: Int -> [String] -> Int
+calc _ (x:_) 
+    | Just val <- readMaybe x = if val > max_brightness
+        then max_brightness
+        else val
+calc val ("-d":_) = darken val
+calc val ("-b":_) = brighten val
+calc val _ = brighten val -- default is brighten
 
 brighten :: Int -> Int
 brighten !val = if val > max_brightness - increment 
