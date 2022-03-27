2b38eba7956692127043761a5468b581a8a9cf57
dan soucy <dev@danso.ca>
2022-03-26 22:45:34 -0400

    update mutt_alias, style changes only

--- mutt_alias/src/mutt_alias.hs
+++ mutt_alias/src/mutt_alias.hs
@@ -1,63 +1,79 @@
-{-# LANGUAGE OverloadedStrings #-}  
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
-{- todo: 
-    clean up the terrible main function.
+{- todo:
     write promptForAlias
     switch from String to Text ?
 -}
 
 
 -- import Data.Text
-import System.Exit
+import System.Exit (exitFailure)
 import Data.Char (toLower, isSpace)
+import Data.Function ((&))
+import Data.Functor ((<&>))
 import Data.List (isPrefixOf, find)
-import Data.Maybe (fromJust)
-import Control.Monad (when, unless)
+import Control.Monad (unless)
 
-data Alias = Alias { alias :: Maybe String, name :: String, address :: String }
-    deriving (Show, Eq)
+data Alias = Alias {
+    alias :: Maybe String,
+    name :: String,
+    address :: Address
+    } deriving (Show, Eq)
+
+newtype Address = Address {
+    from_address :: String
+    } deriving (Show, Eq)
 
 aliasFile :: FilePath
--- aliasFile = "/home/dan/.mutt/aliases.dum.4"
-aliasFile = "/home/dan/.mutt/aliases_hs_test"
+aliasFile = "/home/dan/.mutt/aliases"
 
 main :: IO ()
 main = do
     input <- getContents
-    database <- parse <$> readFile aliasFile
---     print database
-    let maybeSender = fmap fromLine (grepFrom input)
-    when (maybeSender == Nothing) (bail input)
-    let sender = fromJust maybeSender 
-    unless (map toLower (address sender) `elem` database) (writeToFile sender)
-    putStr input
+    database :: [Address] <- readFile aliasFile <&> parse
+    case grepFrom input <&> fromLine of
+        Nothing -> bail input
+        Just (sender :: Alias) -> do
+            unless ((address sender & normalize) `elem` database)
+                   (save sender)
+            putStr input
 
-parse :: String -> [String]
-parse = map ((map toLower) . last . words) . filter (not . all isSpace) . lines
+parse :: String -> [Address]
+parse = lines <&> filter (not . all isSpace) <&> map addr
+    where
+        addr = words <&> last <&> Address <&> normalize
+
+normalize :: Address -> Address
+normalize = from_address <&> map toLower <&> Address
 
 bail :: String -> IO ()
 bail input = putStr input >> exitFailure
 
 grepFrom :: String -> Maybe String
-grepFrom = fmap (fmap removeQuotes) (find (isPrefixOf "From") . lines)
-
-removeQuotes :: String -> String
-removeQuotes = filter ((/=) '"')
+grepFrom = lines <&> find (isPrefixOf "From") <&> fmap removeQuotes
+    where
+        removeQuotes :: String -> String
+        removeQuotes = filter ((/=) '"')
 
 fromLine :: String -> Alias
-fromLine input = Alias Nothing (unwords (init fromline)) (last fromline)
-    where fromline = (drop 1 . words) input
+fromLine input = Alias
+    Nothing
+    (init fromline & unwords)
+    (last fromline & Address)
+        where fromline = (drop 1 . words) input
 
 -- promptForAlias :: Alias -> IO Alias
 -- promptForAlias alias = undefined
 
-writeToFile :: Alias -> IO ()
-writeToFile entry = appendFile aliasFile $ "alias " ++ showAlias entry
+save :: Alias -> IO ()
+save entry = appendFile aliasFile $ "alias " ++ showAlias entry
 
 showAlias :: Alias -> String
-showAlias entry = unwords (ali:name entry:address entry:["\n"])
-    where 
+showAlias entry = unwords (ali:name entry:addr:["\n"])
+    where
         ali :: String
         ali = case alias entry of
-            Nothing -> map toLower $ (filter ((/=) ' ')) (name entry)
+            Nothing -> (filter ((/=) ' ')) (name entry) & map toLower
             Just str -> str
+        addr = address entry & from_address
